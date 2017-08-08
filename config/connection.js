76d4cf3e013930dae27f6c3687ae4cbb846838f5
bdunams <dabadestboy1@gmail.com>
2017-08-07 21:55:10 -0400

    Added DB Config

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,6 @@ const MySQL = require('mysql');
 //Promises.promisifyAll(require("mysql/lib/Connection").prototype);
 //Promises.promisifyAll(require("mysql/lib/Pool").prototype);
 
-mysql://b61ad89695c8ae:b2b97a1e@us-cdbr-iron-east-05.cleardb.net/heroku_bb28dc4dd6f621c?reconnect=true
-
 let burgers = MySQL.createConnection({
 	host: "us-cdbr-iron-east-05.cleardb.net",
 	user: "b61ad89695c8ae",
